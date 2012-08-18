43ec325fe9ec69a99a3fbac92f48f77b92d57f52
Michal Jamroz <michal@yerbamate.com.pl>
2012-08-18 03:43:13 +0200

    a jednak.. ;) nie wszystkie indeksy zaw. trajektorie

--- parsujMoDEL.py
+++ parsujMoDEL.py
@@ -18,8 +18,16 @@ def fetchData(idx):
 
 	data = []
 	d = s.split("\n")
+	jestTraj=False
+	for line in d:
+		if "type=ca" in line:
+			jestTraj=True
+			break
+	if not jestTraj:
+		return []
 
 	for line in d:
+
 		if show and len(line)>2:
 			if "</td>" in line or '</label>' in line:
 				stripped = remove_html_tags(line)
@@ -32,11 +40,11 @@ def fetchData(idx):
 		if showit==11:
 			break
 			
-	iczas = 0
+
 	for i in range(len(d)):
-		if "Simulation time</td>" in d[i]:
-			data.append("Simulation time")
-			data.append(remove_html_tags(d[i+3]))
+		if "<td>Simulation time</td>" in d[i]:
+			data.append("time")
+			data.append(remove_html_tags(d[i+1]))
 			break
 	
 	data.append("http://mmb.pcb.ub.es/MoDEL/servlet/trajectory?idSimulation="+str(idx)+"&type=ca")
@@ -47,11 +55,12 @@ def fetchData(idx):
 fw = open("MoDEL.data.txt","w")
 fw.write("#%3s %10s %10s %5s %80s # %s\n" %("pdb","software","time","len","trajectory","ff"))
 
-for i in range(20,5000):
+for i in range(80,5000):
 	d = fetchData(i)
 	if (len(d)>1): 
-		simlen = int(d[12].split()[0])
+		#simlen = int(d[12].split()[0])
 		
-		if (simlen==10000): # only 10ns
-			fw.write("%4s %10s %10s %5s %80s # %s\n" %(d[0],d[2],d[12],d[10],d[-1],d[4]))
+		#if (simlen==10000): # only 10ns
+#		print "%4s %10s %10s %5s %80s # %s\n" %(d[0],d[2],d[12],d[10],d[-1],d[4])
+		fw.write("%4s %10s %10s %5s %80s # %s\n" %(d[0],d[2],d[12],d[10],d[-1],d[4]))
 fw.close()
